c5e0c02a8fc882e0baa76e09382b2db7705a3541
emrullahonder-6327 <emrullahonder27@gmail.com>
2025-10-29 13:38:38 +0100

    Text geändert 03

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Görkem Makine dikkaaaaaaaaaaaaaaaaa!"
+    return "Görkem ist der Beste!"
 
 # --- NEUER TEIL START ---
 if __name__ == "__main__":

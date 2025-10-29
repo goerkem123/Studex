27d79f1305b1ec4825aad4aa6f7c43bdd5f898ee
VisioN655 <affanatik5565@gmail.com>
2025-10-29 13:24:47 +0100

    Text geändert

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Affan und Emru sind zucker!"
+    return "Görkem Makine dikkaaaaaaaaaaaaaaaaa!"
 
 # --- NEUER TEIL START ---
 if __name__ == "__main__":
